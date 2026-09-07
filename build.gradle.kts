97d921c9999c0f68e167ec3ccd122953c5987356
gokova <gokmenbalkan@gmail.com>
2026-09-07 16:09:41 +0200

    Setup CI/CD configuration for code quality

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,26 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+val catalog = libs
+
+subprojects {
+    pluginManager.apply(catalog.plugins.detekt.get().pluginId)
+    pluginManager.apply(catalog.plugins.ktlint.get().pluginId)
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        jvmTarget = "11"
+    }
+
+    dependencies {
+        "detektPlugins"(catalog.detekt.compose.rules)
+    }
 }
